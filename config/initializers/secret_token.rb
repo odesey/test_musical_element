fb7af4ee1e8b8ea28083bd1deadde6ad94f6d146
Ed <odesey@gmail.com>
2013-05-30 21:52:53 -0400

    initial rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMusicalElement::Application.config.secret_token = 'a6fbd7faa9f9a9c1639179adf0ce7fb756ca7487b7714c1d42e19936e7d949ddb8bccb7ed00960b7b51b4ec88dc4669639badb660838b684d5250d48173b55ad'
